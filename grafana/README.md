e9103d720a656667d2ed72d6971251a4433b65d6
sameeram <mgjaye@gmail.com>
2016-09-22 18:58:16 +0530

    Added mydashboard to be deployed with grafana container

---
+++ grafana/README.md
@@ -0,0 +1,3 @@
+Due to https://github.com/grafana/grafana/issues/2816
+the exported dashboard json needs to be wrapped
+within a { "dashboard":<exported content> }
---
+++ grafana/resources/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+HEADER_CONTENT_TYPE="Content-Type: application/json"
+HEADER_ACCEPT="Accept: application/json"
+GRAFANA_USER=${GRAFANA_USER:-admin}
+GRAFANA_PASSWD=${GRAFANA_PASSWD:-admin}
+GRAFANA_PORT=${GRAFANA_PORT:-3000}
+GF_SERVER_PROTOCOL=${GF_SERVER_PROTOCOL:-http}
+local_ip=localhost
+#$(ip route get 1 | awk '{print $NF;exit}')
+
+# Script adapted from https://github.com/kubernetes/heapster/blob/master/grafana/run.sh
+set -m
+echo "Starting the Garfana Server "
+#exec /usr/sbin/grafana-server start & 
+exec /usr/sbin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana &
+#service grafana-server start
+
+echo "Waiting for Grafana to come up..."
+until $(curl -k --fail --output /dev/null --silent ${GF_SERVER_PROTOCOL}://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/org); do
+  printf "."
+  sleep 2
+done
+echo "Grafana is up and running."
+
+
+echo "Importing default dashboards"
+for filename in /dashboards/*.json; do
+  echo "Importing ${filename} ..."
+  curl -k -i -XPOST --data "@${filename}" -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "${GF_SERVER_PROTOCOL}://${GRAFANA_USER}:${GRAFANA_PASSWD}@${local_ip}:${GRAFANA_PORT}/api/dashboards/db"
+  echo ""
+  echo "Done importing ${filename}"
+done 
+
+fg
+
